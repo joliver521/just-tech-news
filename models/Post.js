8d1ed6793124610627fbf3cffc71cc35875b87a8
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-07-23 11:30:36 -0400

    debugged 13.3 module

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,7 @@
-// expressions
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// create out Post model
+// create our Post model
 class Post extends Model {}
 
 // create fields/columns for Post model
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
 // create our User model
 class User extends Model {
@@ -49,7 +49,7 @@ User.init(
                 );
                 return newUserData;
             },
-            // set up beforeUpdate lifecycle "hook" functionality
+
             async beforeUpdate(updatedUserData) {
                 updatedUserData.password = await bcrypt.hash(
                     updatedUserData.password,
