6a51d2266871d933a6b1bf7512399f075d7c7cdd
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-07-24 11:21:46 -0400

    create the user login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
+    console.log('======================');
     Post.findAll({
         attributes: [
             'id',
@@ -33,20 +35,28 @@ router.get('/', (req, res) => {
             },
             {
                 model: User,
-                attributes: ['Username'],
+                attributes: ['username'],
             },
         ],
     })
         .then(dbPostData => {
-            console.log(dbPostData[0]);
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            // pass a single post object into the homepage template
+
             res.render('homepage', { posts });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
